3fcd5c6fcd887ed80d4ab6e1aa3f1cd18d1942d8
Davide Valeriani <dvaler@essex.ac.uk>
2015-06-11 15:53:53 +0100

    Added ranking

--- server.py
+++ server.py
@@ -10,24 +10,31 @@ from jinja2 import Environment, FileSystemLoader
 from copy import deepcopy as copy
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import datetime
 
 db_name = "users.db"
 base_site = "http://localhost:8080"
 
 env = Environment(loader=FileSystemLoader('templates'))
-
 menu = [{"link": "/", "caption": "Home", "active": False},
         {"link": "/data", "caption": "Get the data", "active": False},
+        {"link": "/rank", "caption": "Leaderboard", "active": False},
         {"link": "/about", "caption": "About", "active": False},
         {"link": "/login", "caption": "Login", "active": False},
         {"link": "/signup", "caption": "Signup", "active": False},
         ]
 menu_logged = [{"link": "/", "caption": "Home", "active": False},
         {"link": "/data", "caption": "Get the data", "active": False},
+        {"link": "/rank", "caption": "Leaderboard", "active": False},
         {"link": "/about", "caption": "About", "active": False},
         {"link": "/submit", "caption": "Submit", "active": False},
         {"link": "/logout", "caption": "Logout", "active": False},
         ]
+
+
+def datetimeformat(value):
+    return datetime.datetime.strptime(value, '%Y-%m-%d %H:%M:%S').strftime('%a, %d %b %Y %H:%M:%S')
+env.filters['datetimeformat'] = datetimeformat
 
 
 def is_logged():
@@ -60,7 +67,7 @@ class About(object):
             current_menu = copy(menu_logged)
         else:
             current_menu = copy(menu)
-        current_menu[2]["active"] = True
+        current_menu[3]["active"] = True
         content = [{"title": "Organizers", "text": (''.join(open("about.html").readlines())).decode('utf-8')}]
         return template.render(navigation=current_menu, content=content)
 
@@ -73,7 +80,7 @@ class Signup(object):
             current_menu = copy(menu_logged)
         else:
             current_menu = copy(menu)
-            current_menu[4]["active"] = True
+            current_menu[5]["active"] = True
         content = [{"title": "Signup", "text": (''.join(open("signup.html").readlines())).decode('utf-8')}]
         return template.render(navigation=current_menu, content=content)
 
@@ -121,11 +128,8 @@ class Signup(object):
             "<p>Please click on this link to complete your registration <a href='"+link+"'>"+link+"</a></p>" \
             "<p>Best regards<br>CEEC 2015 Programme Committee</p></body></html>"
         # Record the MIME types of both parts - text/plain and text/html.
-        part1 = MIMEText(text, 'plain')
-        part2 = MIMEText(html, 'html')
-        # Attach parts into message container.
-        msg.attach(part1)
-        msg.attach(part2)
+        msg.attach(MIMEText(text, 'plain'))
+        msg.attach(MIMEText(html, 'html'))
         # Send the message
         server = smtplib.SMTP("smtp.123-reg.co.uk:587")
         server.login("competition@ceec.uk", "cherryPy123__")
@@ -139,7 +143,7 @@ class Signup(object):
         # Build the page to show
         template = env.get_template("template.html")
         current_menu = copy(menu)
-        current_menu[4]["active"] = True
+        current_menu[5]["active"] = True
         content = [{"title": "Account created!",
                     "text": "Check your email to activate your account and start competing!"}]
         return template.render(navigation=current_menu, content=content)
@@ -181,7 +185,7 @@ class Login(object):
             current_menu = copy(menu_logged)
         else:
             current_menu = copy(menu)
-            current_menu[3]["active"] = True
+            current_menu[4]["active"] = True
         content = [{"title": "Login", "text": (''.join(open("login.html").readlines())).decode('utf-8')}]
         return template.render(navigation=current_menu, content=content)
 
@@ -291,7 +295,7 @@ class Submit(object):
         if "id" in cherrypy.session:
             template = env.get_template("userdata.html")
             current_menu = copy(menu_logged)
-            current_menu[3]["active"] = True
+            current_menu[4]["active"] = True
             content = [{"title": "Submit an entry", "text": (''.join(open("submit.html").readlines())).decode('utf-8')}]
             user = {"name": cherrypy.session["name"]}
             return template.render(navigation=current_menu, content=content, user=user)
@@ -301,24 +305,50 @@ class Submit(object):
     @cherrypy.expose
     def make_submission(self, entry):
         # Save the file
-        filename = str(cherrypy.session["id"])+"_"+time.strftime("%Y-%m-%d_%H:%M:%S", time.gmtime())+".txt"
+        date = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+        filename = str(cherrypy.session["id"])+"_"+date.replace(" ", "_")+".txt"
         output = open("submissions/"+filename, "w")
         output.write(entry.file.read())
         output.close()
         # TODO Process the file to compute the score
         score = random.random()
+        # Store the score in the database
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        c.execute("INSERT INTO submissions(author, date, score) VALUES(?, ?, ?)",
+                  [cherrypy.session["id"], date, score])
+        conn.commit()
+        conn.close()
         score_text = "This submission has achieved a score of <strong>"+str(score)+"</strong> on the validation set." \
                                                                                    "<br><a class='btn btn-success' href='/rank'>Check the rank</a>"
         # Build the page to show
         template = env.get_template("userdata.html")
         current_menu = copy(menu_logged)
-        current_menu[3]["active"] = True
+        current_menu[4]["active"] = True
         content = [{"title": "Submitted!",
                     "text": (''.join(open("submission.html").readlines())).decode('utf-8')},
                    {"title": "Score",
                     "text": score_text}]
         user = {"name": cherrypy.session["name"]}
         return template.render(navigation=current_menu, content=content, user=user)
+
+
+class Rank(object):
+    @cherrypy.expose
+    def index(self):
+        conn = sqlite3.connect(db_name)
+        c = conn.cursor()
+        c.execute("SELECT u.username, s.date, s.score FROM users as u, submissions as s WHERE u.id = s.author ORDER BY s.score DESC, s.date DESC")
+        res = c.fetchall()
+        for subm in res:
+            print(subm)
+        template = env.get_template("template_rank.html")
+        if is_logged():
+            current_menu = copy(menu_logged)
+        else:
+            current_menu = copy(menu)
+        current_menu[2]["active"] = True
+        return template.render(navigation=current_menu, submissions=res)
 
 
 def get_users():
@@ -343,5 +373,6 @@ if __name__ == '__main__':
     root.logout = Logout()
     root.signup = Signup()
     root.submit = Submit()
+    root.rank = Rank()
     root.activate = Activate()
     cherrypy.quickstart(root, '/', config=conf)
