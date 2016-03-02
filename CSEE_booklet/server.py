b6b10d93d18a11e669a89acb73268841ca4b4bf7
Davide Valeriani <dvaler@essex.ac.uk>
2016-03-02 18:39:40 +0000

    Updated the booklet server in order to support the template used by the students.

--- CSEE_booklet/server.py
+++ CSEE_booklet/server.py
@@ -13,6 +13,8 @@ from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
 import xlrd
 
+use_antiword = False
+
 class HomePage(object):
     @cherrypy.expose
     def index(self):
@@ -106,37 +108,116 @@ class HomePage(object):
         # For each abstract form
         for dirname, dirnames, filenames in os.walk('tmp/'):
             for f in filenames:
-                try:
-                    # Convert to txt
-                    txt_file = subprocess.check_output(["antiword", "tmp/%s" % f])
-                except:
+                # Rename file to unix-like format
+                os.rename("tmp/"+f, "tmp/"+f.replace(" ", "-").lower())
+                f = f.replace(" ", "-").lower()
+                project_title = ""
+                student_first = ""
+                student_last = ""
+                supervisor_first = ""
+                supervisor_last = ""
+                degree = ""
+                abstract = ""
+                if not use_antiword:
+                    document = Document("tmp/"+f)
+                    abstract_begin = False
+                    try:
+                        for p in document.paragraphs:
+                            p = p.text
+                            if "Project Title" in p:
+                                try:
+                                    project_title = p.split("Project Title:")[1].lstrip()
+                                    project_title = ' '.join(project_title.split())
+                                except:
+                                    project_title = p
+                            elif "Student First Name(s)" in p:
+                                try:
+                                    student_first = p.split("Student First Name(s):")[1].lstrip()
+                                    student_first = ' '.join(student_first.split())
+                                except:
+                                    print "WARNING: check", project_title
+                                    student_first = p
+                            elif "Student Surname" in p:
+                                try:
+                                    student_last = p.split("Student Surname:")[1].lstrip()
+                                    student_last = ' '.join(student_last.split())
+                                except:
+                                    print "WARNING: check", project_title
+                                    student_last = p
+                            elif "Supervisor First" in p:
+                                try:
+                                    supervisor_first = p.split("Supervisor First Name(s):")[1].lstrip()
+                                    supervisor_first = ' '.join(supervisor_first.split())
+                                except:
+                                    print "WARNING: check", project_title
+                                    supervisor_first = p
+                            elif "Supervisor Surname" in p:
+                                try:
+                                    supervisor_last = p.split("Supervisor Surname:")[1].lstrip()
+                                    supervisor_last = ' '.join(supervisor_last.split())
+                                except:
+                                    print "WARNING: check", project_title
+                                    supervisor_last = p
+                            elif "Degree Course" in p:
+                                try:
+                                    degree = p.split("Degree Course:")[1].lstrip()
+                                    degree = ' '.join(degree.split())
+                                except:
+                                    print "WARNING: check", project_title
+                                    degree = p
+                            elif "Abstract." not in p and abstract_begin:
+                                abstract += p
+                            elif "Abstract." in p:
+                                try:
+                                    abstract = p.split("Abstract.")[1].lstrip()
+                                except:
+                                    print "WARNING: check", project_title
+                                    abstract = p
+                                abstract_begin = True
+                    except:
+                        print f
+
+                else:
+                    try:
+                        # Convert to txt
+                        txt_file = subprocess.check_output(["antiword", "tmp/%s" % f])
+                    except:
+                        continue
+                    counter = 0
+                    for field in txt_file.split("\n"):
+                        field = field.lstrip()
+                        if "Project Title" in field:
+                            project_title = field[len("Project Title:"):].lstrip()
+                        elif "Student" in field:
+                            student = field[len("Student:"):].lstrip()
+                        elif "Supervisor" in field:
+                            supervisor = field[len("Supervisor:"):].lstrip()
+                        elif "Abstract." in field:
+                            abstract = field[len("Abstract."):].lstrip()
+                            abstract += "\n".join(txt_file.split("\n")[counter+1:])
+                            break
+                        counter += 1
+                    # get the degree from the students_list
+                    reformatted_student_name = (student.split(" ")[-1]+", "+" ".join(student.split(" ")[:-1])).upper()
+                    try:
+                        degree = students[reformatted_student_name]
+                    except:
+                        degree = ""
+                if not all([project_title, student_first, student_last, supervisor_first, supervisor_last, degree]):
+                    print "WARNING: file %s not inserted" % f
                     continue
-                counter = 0
-                for field in txt_file.split("\n"):
-                    field = field.lstrip()
-                    if "Project Title" in field:
-                        project_title = field[len("Project Title:"):].lstrip()
-                    elif "Student" in field:
-                        student = field[len("Student:"):].lstrip()
-                    elif "Supervisor" in field:
-                        supervisor = field[len("Supervisor:"):].lstrip()
-                    elif "Abstract." in field:
-                        abstract = field[len("Abstract."):].lstrip()
-                        abstract += "\n".join(txt_file.split("\n")[counter+1:])
-                        break
-                    counter += 1
-                # get the degree from the students_list
-                reformatted_student_name = (student.split(" ")[-1]+", "+" ".join(student.split(" ")[:-1])).upper()
-                try:
-                    degree = students[reformatted_student_name]
-                except:
-                    degree = ""
+                print "TITLE:", project_title, f
+                #print "STUDENT:", student_first, student_last
+                #print "SUPERVISOR:", supervisor_first, supervisor_last
+                #print "DEGREE COURSE:", degree
+                #print "ABSTRACT:", abstract
+                # Adding to the main document
                 heading = booklet_doc.add_paragraph()
                 paragraph_format = heading.paragraph_format
-                run = heading.add_run(project_title)
+                run = heading.add_run("\n"+project_title.title())
                 run.bold = True
                 font = run.font
-                font.size = Pt(16)
+                font.size = Pt(13)
                 paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
                 table = booklet_doc.add_table(3, 2)
                 table.allow_autofit = False
@@ -149,13 +230,20 @@ class HomePage(object):
                     trHeight.set(qn('w:hRule'), "exact")
                     trPr.append(trHeight)
                 table.cell(0, 0).paragraphs[0].add_run('Name:').bold = True
-                table.cell(0, 1).paragraphs[0].add_run(student).bold = True
+                table.cell(0, 1).paragraphs[0].add_run(student_first.title()+" "+student_last.title()).bold = True
                 table.cell(1, 0).paragraphs[0].add_run('Degree course:').bold = True
-                table.cell(1, 1).paragraphs[0].add_run(degree).bold = True
+                table.cell(1, 1).paragraphs[0].add_run(degree.title()).bold = True
                 table.cell(2, 0).paragraphs[0].add_run('Supervisor:').bold = True
-                table.cell(2, 1).paragraphs[0].add_run(supervisor).bold = True
+                table.cell(2, 1).paragraphs[0].add_run(supervisor_first.title()+" "+supervisor_last.title()).bold = True
                 abstract_paragraph = booklet_doc.add_paragraph()
-                abstract_paragraph.add_run('Abstract.\n'+abstract).bold = True
+                run = abstract_paragraph.add_run('\nAbstract.\n\n')
+                run.bold = True
+                font = run.font
+                font.size = Pt(12)
+                run = abstract_paragraph.add_run(abstract)
+                run.bold = False
+                font = run.font
+                font.size = Pt(12)
         # Save the report
         booklet_doc.save("booklet.docx")
 
