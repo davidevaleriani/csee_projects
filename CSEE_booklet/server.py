6a8e92af5303ba8ddcc794f09177ff10c3238238
Davide Valeriani <dvaler@essex.ac.uk>
2016-03-03 23:39:06 +0000

    Further modifications to use a CSV file of students as a backup to complete missing information. Added also a brief report after generating the booklet.

--- CSEE_booklet/server.py
+++ CSEE_booklet/server.py
@@ -12,6 +12,8 @@ from docx.shared import Pt, Cm
 from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
 import xlrd
+import csv
+import numpy as np
 
 use_antiword = False
 
@@ -63,9 +65,9 @@ class HomePage(object):
                     <br>
                     <form method="post" action="get_booklet" enctype="multipart/form-data" class="form-horizontal">
                         <div class="form-group">
-                            <label for="marks" class="col-xs-2 col-xs-offset-2 control-label">Zip file</label>
+                            <label for="abstracts" class="col-xs-2 col-xs-offset-2 control-label">Zip file</label>
                             <div class="col-xs-6">
-                                <input type="file" name="marks" id="marks" />
+                                <input type="file" name="abstracts" id="abstracts" />
                             </div>
                         </div>
                         <div class="form-group">
@@ -87,30 +89,47 @@ class HomePage(object):
             """
 
     @cherrypy.expose
-    def get_booklet(self, marks):
-        # Get list of students
+    def get_booklet(self, abstracts):
+        # Get list of students for "backup"
         students = {}
-        workbook = xlrd.open_workbook("students_list.xls")
-        worksheets = workbook.sheet_names()
-        worksheet = workbook.sheet_by_name(worksheets[0])
-        for row in range(1, worksheet.nrows):
-            students[worksheet.cell_value(row, 0)] = worksheet.cell(row, 2)
+        if os.path.exists("CE301LST.csv"):
+            # Load list of students from CSV file
+            # Full name, first names, degree, regno, surname
+            with open('CE301LST.csv', 'rb') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                for row in spamreader:
+                    # strudents[regno] = (first names, surname, degree)
+                    students[row[3]] = (row[1], row[4], row[2])
+        elif os.path.exists("students_list.xls"):
+            # Load list of students from Excel file
+            workbook = xlrd.open_workbook("students_list.xls")
+            worksheets = workbook.sheet_names()
+            worksheet = workbook.sheet_by_name(worksheets[0])
+            for row in range(1, worksheet.nrows):
+                students[worksheet.cell_value(row, 0)] = worksheet.cell(row, 2)
+        # Create the directory where the abstract will be temporarily saved
         if not os.path.isdir("tmp"):
             os.mkdir("tmp")
-        # Extract files
-        if type(marks) != file and hasattr(marks, "file"):
-            marks = marks.file
-        zipf = zipfile.ZipFile(marks, 'r')
+        # Extract files from the zip file
+        if type(abstracts) != file and hasattr(abstracts, "file"):
+            abstracts = abstracts.file
+        zipf = zipfile.ZipFile(abstracts, 'r')
         zipf.extractall("tmp/")
         # Init the booklet document
         booklet_doc = Document("booklet_template.docx")
         booklet_doc.add_page_break()
+        processed_files = []
+        files_not_inserted = []
+        files_to_be_checked = []
         # For each abstract form
         for dirname, dirnames, filenames in os.walk('tmp/'):
             for f in filenames:
-                # Rename file to unix-like format
+                # Rename file to unix-like format (remove spaces, etc.)
                 os.rename("tmp/"+f, "tmp/"+f.replace(" ", "-").lower())
                 f = f.replace(" ", "-").lower()
+                if f in processed_files:
+                    continue
+                processed_files.append(f)
                 project_title = ""
                 student_first = ""
                 student_last = ""
@@ -118,9 +137,16 @@ class HomePage(object):
                 supervisor_last = ""
                 degree = ""
                 abstract = ""
-                if not use_antiword:
+                abstract_has_started = False
+                if f[-4:] == "docx":
+                    # If the abstracts are in .docx format
                     document = Document("tmp/"+f)
                     abstract_begin = False
+                    # Get the degree course and the student names from the students list
+                    if unicode(f[:7], 'utf-8').isnumeric() and f[:7] in students.keys():
+                        student_first = students[f[:7]][0].title()
+                        student_last = students[f[:7]][1].title()
+                        degree = students[f[:7]][2].title()
                     try:
                         for p in document.paragraphs:
                             p = p.text
@@ -129,59 +155,101 @@ class HomePage(object):
                                     project_title = p.split("Project Title:")[1].lstrip()
                                     project_title = ' '.join(project_title.split())
                                 except:
+                                    print "WARNING: check title of", project_title
+                                    if f in processed_files:
+                                        processed_files.remove(f)
+                                    if f not in files_to_be_checked:
+                                        files_to_be_checked.append(f)
                                     project_title = p
-                            elif "Student First Name(s)" in p:
+                            elif "Student First Name" in p and not student_first:
                                 try:
-                                    student_first = p.split("Student First Name(s):")[1].lstrip()
+                                    if len(p.split("Student First Name(s):")) > 1:
+                                        student_first = p.split("Student First Name(s):")[1].lstrip()
+                                    elif len(p.split("Student First Name:")) > 1:
+                                        student_first = p.split("Student First Name:")[1].lstrip()
+                                    else:
+                                        raise
                                     student_first = ' '.join(student_first.split())
                                 except:
-                                    print "WARNING: check", project_title
+                                    print "WARNING: check student name of", project_title
+                                    if f in processed_files:
+                                        processed_files.remove(f)
+                                    if f not in files_to_be_checked:
+                                        files_to_be_checked.append(f)
                                     student_first = p
-                            elif "Student Surname" in p:
+                            elif "Student Surname" in p and not student_last:
                                 try:
                                     student_last = p.split("Student Surname:")[1].lstrip()
                                     student_last = ' '.join(student_last.split())
                                 except:
-                                    print "WARNING: check", project_title
+                                    print "WARNING: check student name of", project_title
+                                    if f in processed_files:
+                                        processed_files.remove(f)
+                                    if f not in files_to_be_checked:
+                                        files_to_be_checked.append(f)
                                     student_last = p
                             elif "Supervisor First" in p:
                                 try:
-                                    supervisor_first = p.split("Supervisor First Name(s):")[1].lstrip()
+                                    if len(p.split("Supervisor First Name(s):")) > 1:
+                                        supervisor_first = p.split("Supervisor First Name(s):")[1].lstrip()
+                                    elif len(p.split("Supervisor First Name:")) > 1:
+                                        supervisor_first = p.split("Supervisor First Name:")[1].lstrip()
+                                    else:
+                                        raise
                                     supervisor_first = ' '.join(supervisor_first.split())
                                 except:
-                                    print "WARNING: check", project_title
+                                    print "WARNING: check supervisor of", project_title
+                                    print p
+                                    if f in processed_files:
+                                        processed_files.remove(f)
+                                    if f not in files_to_be_checked:
+                                        files_to_be_checked.append(f)
                                     supervisor_first = p
                             elif "Supervisor Surname" in p:
                                 try:
                                     supervisor_last = p.split("Supervisor Surname:")[1].lstrip()
                                     supervisor_last = ' '.join(supervisor_last.split())
                                 except:
-                                    print "WARNING: check", project_title
+                                    print "WARNING: check supervisor of", project_title
+                                    if f in processed_files:
+                                        processed_files.remove(f)
+                                    if f not in files_to_be_checked:
+                                        files_to_be_checked.append(f)
                                     supervisor_last = p
-                            elif "Degree Course" in p:
+                            elif "Degree Course" in p and not degree:
                                 try:
                                     degree = p.split("Degree Course:")[1].lstrip()
                                     degree = ' '.join(degree.split())
                                 except:
-                                    print "WARNING: check", project_title
+                                    print "WARNING: check degree of", project_title
+                                    if f in processed_files:
+                                        processed_files.remove(f)
+                                    if f not in files_to_be_checked:
+                                        files_to_be_checked.append(f)
                                     degree = p
-                            elif "Abstract." not in p and abstract_begin:
+                            elif "Abstract." not in p and abstract_has_started:
                                 abstract += p
                             elif "Abstract." in p:
                                 try:
                                     abstract = p.split("Abstract.")[1].lstrip()
+                                    abstract_has_started = True
                                 except:
-                                    print "WARNING: check", project_title
+                                    print "WARNING: check abstract of", project_title
+                                    if f in processed_files:
+                                        processed_files.remove(f)
+                                    if f not in files_to_be_checked:
+                                        files_to_be_checked.append(f)
                                     abstract = p
-                                abstract_begin = True
                     except:
-                        print f
-
-                else:
+                        print "ERROR with file", f, "- SKIP"
+                        continue
+                elif f[-3:] == "doc":
+                    # If the abstracts are in .doc format
                     try:
-                        # Convert to txt
+                        # Convert the .doc to .txt
                         txt_file = subprocess.check_output(["antiword", "tmp/%s" % f])
                     except:
+                        print "WARNING: unable to convert", f, "- SKIP"
                         continue
                     counter = 0
                     for field in txt_file.split("\n"):
@@ -190,8 +258,12 @@ class HomePage(object):
                             project_title = field[len("Project Title:"):].lstrip()
                         elif "Student" in field:
                             student = field[len("Student:"):].lstrip()
+                            student_first = student.split()[:-1]
+                            student_last = student.split()[-1]
                         elif "Supervisor" in field:
                             supervisor = field[len("Supervisor:"):].lstrip()
+                            supervisor_first = supervisor.split()[:-1]
+                            supervisor_last = supervisor.split()[-1]
                         elif "Abstract." in field:
                             abstract = field[len("Abstract."):].lstrip()
                             abstract += "\n".join(txt_file.split("\n")[counter+1:])
@@ -205,12 +277,15 @@ class HomePage(object):
                         degree = ""
                 if not all([project_title, student_first, student_last, supervisor_first, supervisor_last, degree]):
                     print "WARNING: file %s not inserted" % f
+                    processed_files.remove(f)
+                    files_not_inserted.append(f)
+                    print "TITLE:", project_title
+                    print "STUDENT:", student_first, student_last
+                    print "SUPERVISOR:", supervisor_first, supervisor_last
+                    print "DEGREE COURSE:", degree
+                    print "ABSTRACT:", abstract
+                    print
                     continue
-                print "TITLE:", project_title, f
-                #print "STUDENT:", student_first, student_last
-                #print "SUPERVISOR:", supervisor_first, supervisor_last
-                #print "DEGREE COURSE:", degree
-                #print "ABSTRACT:", abstract
                 # Adding to the main document
                 heading = booklet_doc.add_paragraph()
                 paragraph_format = heading.paragraph_format
@@ -219,7 +294,7 @@ class HomePage(object):
                 font = run.font
                 font.size = Pt(13)
                 paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-                table = booklet_doc.add_table(3, 2)
+                table = booklet_doc.add_table(3, 3)
                 table.allow_autofit = False
                 for i in range(3):
                     row = table.rows[i]
@@ -236,7 +311,7 @@ class HomePage(object):
                 table.cell(2, 0).paragraphs[0].add_run('Supervisor:').bold = True
                 table.cell(2, 1).paragraphs[0].add_run(supervisor_first.title()+" "+supervisor_last.title()).bold = True
                 abstract_paragraph = booklet_doc.add_paragraph()
-                run = abstract_paragraph.add_run('\nAbstract.\n\n')
+                run = abstract_paragraph.add_run('\nAbstract.\n')
                 run.bold = True
                 font = run.font
                 font.size = Pt(12)
@@ -250,10 +325,30 @@ class HomePage(object):
         # Remove the marks
         shutil.rmtree('tmp')
 
+        return """
+        <html>
+        <body>
+        <h2>Files added: %d</h2>
+        <h2>Files to be checked: %d</h2>
+        <h3><ul>%s</ul><h3>
+        <h2>Files not inserted: %d</h2>
+        <h3><ul>%s</ul><h3>
+        <h2>Total: %d</h2>
+        <h1 align="center"><a href="download_booklet">Download booklet</a></h1>
+        </body>
+        </html>
+        """ % (len(processed_files),
+               len(files_to_be_checked),
+               ''.join(["<li>"+filename+"</li>" for filename in files_to_be_checked]),
+               len(files_not_inserted),
+               ''.join(["<li>"+filename+"</li>" for filename in files_not_inserted]),
+               len(processed_files+files_not_inserted+files_to_be_checked))
+
+    @cherrypy.expose
+    def download_booklet(self):
         # Return the file to download
         booklet_doc = open("booklet.docx", 'r')
         return cherrypy.lib.static.serve_fileobj(booklet_doc, disposition='attachment', name="booklet.docx")
-
 
 if __name__ == '__main__':
     cherrypy.server.socket_host = '0.0.0.0'
