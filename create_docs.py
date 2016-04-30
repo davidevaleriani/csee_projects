e4e25bca2ea97e57226c9734f74702d7241fc459
Davide Valeriani <dvaler@essex.ac.uk>
2016-04-30 13:18:42 +0100

    We can now mark the reflective statements by filling a unique spreadsheet!

--- create_docs.py
+++ create_docs.py
@@ -30,74 +30,106 @@ import shutil
 
 # CONFIGURATION
 
-template_filename = "template_rs.docx"     # Model of the docx file to fill
-csv_filename = "ce101_2016.csv"        # List of students of CE101 (registration number; surname, first name(s); .....)
-feedback_directory = "feedback/"        # Directory where the filled feedback documents will be saved
-marks_filename = "marks.csv"            # registration number;mark1;mark2...
+# Type of document (rs = Reflective Statement; tr = Team Report)
+type_of_document = "rs"
+# Main working directory
+main_folder = ""
+# Model of the docx file to fill
+template_filename = "CE101 Reflective Statement Feedback Form 2015.docx"
+# List of students of CE101 (registration number; surname, first name(s); mark1; mark2; ...)
+csv_filename = "ce101_2016.csv"
+# Directory where the filled feedback documents will be saved
+feedback_directory = type_of_document+"/"
+# Columns settings (in the CSV file)
+regno_col = 0
+fullname_col = 1
+mark1_col = 2
+group_name_col = 3
+num_options_available_marks = [2, 4, 2, 3, 3, 3, 2, 2]
 
-if len(sys.argv) > 3:
-    print("Using: python %s [docx template] [CSV list of students]" % (sys.argv[0]))
-    exit(1)
-elif len(sys.argv) == 3:
-    template_filename = sys.argv[1]
-    csv_filename = sys.argv[2]
-elif len(sys.argv) == 2:
-    template_filename = sys.argv[1]
-else:
-    print("INFO: Using default %s and %s files" % (template_filename, csv_filename))
 # MAIN
 
 # Check if the template is accessible
-if not os.path.isfile(csv_filename):
-    print("Error: unable to open the file", csv_filename)
+if not os.path.isfile(main_folder+csv_filename):
+    print("Error: unable to open the file", main_folder+csv_filename)
     exit(1)
-csv_file = open(csv_filename, 'rU')
-counter = 0
+csv_file = open(main_folder+csv_filename, 'rU')
+files_created = 0
 for row in csv.reader(csv_file, delimiter=",", quotechar='"'):
     # Skip the header row
     try:
-        reg_number = int(row[0])
+        reg_number = int(row[regno_col])
     except:
         continue
     # Skip empty rows
-    if row[0] == "":
+    if row[regno_col] == "":
         continue
     # Extract information about the student
     #surname = row[1].split(" ")[0]
     #first_name = row[1].split(",")[1]
-    full_name = row[3]
-    group_name = row[-1].split("Group ")[1]
+    full_name = row[fullname_col]
+    if type_of_document == "tr":
+        group_name = row[-1].split("Group ")[1]
+        output_filename = str(reg_number)+"_Group_"+group_name+"_Team_Report_Feedback.docx"
+    elif type_of_document == "rs":
+        output_filename = str(reg_number)+"_Reflective_Statement.docx"
+    else:
+        print "Type of document not supported"
+        exit(1)
     print("Processing", reg_number, full_name)
-    #output_filename = str(reg_number)+"_Group_"+group_name+"_Team_Report_Feedback.docx"
-    output_filename = str(reg_number)+"_Reflective_Statement.docx"
     # Create the directory to store feedback
-    if not os.path.isdir(feedback_directory):
-        os.makedirs(feedback_directory)
+    if not os.path.isdir(main_folder+feedback_directory):
+        os.makedirs(main_folder+feedback_directory)
     # Check if the template is accessible
-    if not os.path.isfile(template_filename):
-        print("Error: unable to open the file", template_filename)
+    if not os.path.isfile(main_folder+template_filename):
+        print("Error: unable to open the file", main_folder+template_filename)
         exit(1)
     else:
         # Open template docx
-        zip = zipfile.ZipFile(template_filename)
+        zip = zipfile.ZipFile(main_folder+template_filename)
         # Read the xml document, that is basically the "proper" document file
         word_xml = zip.read('word/document.xml')
         # Get the XML tree
         tree = etree.fromstring(word_xml)
         word_schema = 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'
         # Add the name of the student near the Name label
+        if type_of_document == "rs":
+            current_mark_index = 0
+            skip_checkbox = 0
+            if row[mark1_col+current_mark_index] == "":
+                current_mark = num_options_available_marks[current_mark_index]-1
+                enable_marking = False
+            else:
+                current_mark = int(row[mark1_col+current_mark_index])
+                enable_marking = True
         for node in tree.iter(tag=etree.Element):
             if node.tag == '{%s}%s' % (word_schema, 't'):
                 if "Name:" in node.text:
                     node.text = "Name: "+full_name
                 if len(node.text) == 1:
                     node.text = ""
-            if node.tag == '{%s}%s' % (word_schema, 'checkBox'):
-                for c in node.iterchildren():
-                    if c.tag == '{%s}%s' % (word_schema, 'default'):
-                        #c.set(c.keys()[0], "1")
-                        pass
-
+            if type_of_document == "rs":
+                # Fill reflective statement with the marks
+                if node.tag == '{%s}%s' % (word_schema, 'checkBox'):
+                    for c in node.iterchildren():
+                        if c.tag == '{%s}%s' % (word_schema, 'default'):
+                            if skip_checkbox > 0:
+                                skip_checkbox -= 1
+                            elif current_mark == 0:
+                                if enable_marking:
+                                    c.set(c.keys()[0], "1")
+                                    skip_checkbox = num_options_available_marks[current_mark_index] - int(row[mark1_col+current_mark_index]) - 1
+                                current_mark_index += 1
+                                if (mark1_col + current_mark_index) >= len(row):
+                                    continue
+                                if row[mark1_col + current_mark_index] == "":
+                                    current_mark = num_options_available_marks[current_mark_index] - 1
+                                    enable_marking = False
+                                else:
+                                    current_mark = int(row[mark1_col + current_mark_index])
+                                    enable_marking = True
+                            else:
+                                current_mark -= 1
 
         # Make temporary directory
         tmp_dir = tempfile.mkdtemp()
@@ -113,12 +145,12 @@ for row in csv.reader(csv_file, delimiter=",", quotechar='"'):
         filenames = zip.namelist()
         # Now, create the new zip file and add all the filex into the archive
         zip_copy_filename = output_filename
-        with zipfile.ZipFile(feedback_directory+zip_copy_filename, "w") as docx:
+        with zipfile.ZipFile(main_folder+feedback_directory+zip_copy_filename, "w") as docx:
             for filename in filenames:
                 docx.write(os.path.join(tmp_dir, filename), filename)
 
         # Clean up the temp dir
         shutil.rmtree(tmp_dir)
-    counter += 1
+        files_created += 1
 
-print("Process finished:", counter, "file created")
+print("Process finished:", files_created, "file created")
